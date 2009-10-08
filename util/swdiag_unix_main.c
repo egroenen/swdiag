e4c5454fecb26134cbca641c1e88618bb68662b2
Edward Groenendaal <eddyg@myreflection.org>
2009-10-08 00:04:24 +0000

    Wrong license on these two files, fixed.

--- util/swdiag_unix_main.c
+++ util/swdiag_unix_main.c
@@ -1,26 +1,27 @@
 /* 
- *  swdiag_unix_main.c
+ * swdiag_unix_main.c
  *
- *     ``The contents of this file are subject to the Mozilla Public License
- *   Version 1.1 (the "License"); you may not use this file except in
- *   compliance with the License. You may obtain a copy of the License at
- *   http://www.mozilla.org/MPL/
+ * Copyright (c) 2007-2009 Cisco Systems Inc.
  *
- *   Software distributed under the License is distributed on an "AS IS"
- *   basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
- *   License for the specific language governing rights and limitations
- *   under the License.
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use, copy,
+ * modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
  *
- *   The Original Code is libswdiag.
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
  *
- *   The Initial Developer of the Original Code is Cisco Systems Inc.
- *   Portions created by Cisco Systems Inc are 
- *   Copyright (C) Cisco Systems Inc. 2007-2009. All Rights Reserved.
- *
- *   Contributor(s): 
- *        Edward Groenendaal     eddyg@myreflection.org
- *        Brett Wollard          brettw@cisco.com
- *        Chandra Wagh           cwagh@cisco.com
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 
 /*
